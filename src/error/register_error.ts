3a3a07922866752fb0552e54d5b7fbdfbf5e9801
TheNonStopGamer <rickopdekop@gmail.com>
2021-12-30 11:49:19 +0100

    Fixed registerError

--- src/error/register_error.ts
+++ src/error/register_error.ts
@@ -11,7 +11,7 @@ readFile('./logs/errors.json', 'utf8', (err, data) => {
 });
 
 export function registerError(error: CommanderError): void {
-	if (error.code in errorLogData) errorLogData[error.code] = 1;
+	if (!(error.code in errorLogData)) errorLogData[error.code] = 1;
 	else errorLogData[error.code]!++;
 
 	writeFile(
