043586dc5d142c9941c7bd56b71891141352d1a6
Jaren Goldberg <fyrlex@gmail.com>
2023-01-11 12:24:59 -0500

    fix: catch error file

--- src/error/base.ts
+++ src/error/base.ts
@@ -1,21 +1,31 @@
 import { BaseExceptionData } from '../typings/error.js';
-import { readFileSync, writeFile } from 'fs';
+import { existsSync, readFileSync, writeFile } from 'fs';
 
 export abstract class BaseException<Errors extends { [K in string]: (...args: never[]) => BaseExceptionData }, K extends keyof Errors> extends Error {
-	private static readonly errorLogData: Record<string, number> = JSON.parse(readFileSync('./logs/errors.json', 'utf8'));
+	private static readonly errorLogData: Record<string, number> = this.getErrorLog();
 
 	public readonly code: string;
 
 	constructor(name: K, ...args: Parameters<Errors[K]>) {
 		super();
 		const data = this.getErrorFunc(name)(...args);
+
 		this.message = data.message;
 		this.name = `CommanderError [${String(name)}] (${data.code})`;
 		this.code = data.code;
 
+
 		if (Error.captureStackTrace) Error.captureStackTrace(this, this.constructor);
 
 		this.register();
+	}
+
+	private static getErrorLog(): Record<string, number> {
+		if (!existsSync('./logs/errors.json')) {
+			writeFile('./logs/errors.json', JSON.stringify({}), (err) => { if (err) throw err; });
+		}
+
+		return JSON.parse(readFileSync('./logs/errors.json', 'utf-8'));
 	}
 
 	private register(): void {
