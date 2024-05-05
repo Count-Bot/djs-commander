ed34e6bb644936d13807932744570a9211ec8f67
Jaren Goldberg <fyrlex@gmail.com>
2024-05-05 19:20:19 -0400

    fix: remove log writing

--- src/error/base.ts
+++ src/error/base.ts
@@ -1,12 +1,9 @@
 import { BaseExceptionData } from '../typings/error.js';
-import { existsSync, readFileSync, writeFile } from 'fs';
 
 export abstract class BaseException<Errors extends { [K in string]: (...args: never[]) => BaseExceptionData }, K extends keyof Errors> extends Error {
-	private static readonly errorLogData: Record<string, number> = this.getErrorLog();
-
 	public readonly code: string;
 
-	constructor(name: K, ...args: Parameters<Errors[K]>) {
+	constructor (name: K, ...args: Parameters<Errors[K]>) {
 		super();
 		const data = this.getErrorFunc(name)(...args);
 
@@ -15,28 +12,9 @@ export abstract class BaseException<Errors extends { [K in string]: (...args: ne
 		this.code = data.code;
 
 
-		if (Error.captureStackTrace) Error.captureStackTrace(this, this.constructor);
-
-		this.register();
-	}
-
-	private static getErrorLog(): Record<string, number> {
-		if (!existsSync('./logs/errors.json')) {
-			writeFile('./logs/errors.json', JSON.stringify({}), (err) => { if (err) throw err; });
+		if (Error.captureStackTrace) {
+			Error.captureStackTrace(this, this.constructor);
 		}
-
-		return JSON.parse(readFileSync('./logs/errors.json', 'utf-8'));
-	}
-
-	private register(): void {
-		BaseException.errorLogData[this.code] ??= 0;
-		BaseException.errorLogData[this.code]++;
-
-		writeFile(
-			'./logs/errors.json',
-			JSON.stringify(BaseException.errorLogData),
-			(err) => { if (err) throw err; }
-		);
 	}
 
 	protected abstract getErrorFunc(name: K): Errors[K];
