72cc3c3e8c3de94da8108dc7c80a6dac343a145f
Jaren Goldberg <fyrlex@gmail.com>
2025-03-08 18:52:30 -0500

    feat: jsdoc

--- src/handler/AppCommandHandler.ts
+++ src/handler/AppCommandHandler.ts
@@ -1,19 +1,42 @@
 import ky from 'ky';
 import { AppCommandType } from '../models/AppCommandShape.js';
 import { CommandUpdateError, InvalidAppCommandError } from '../models/errors.js';
-import { Router } from '../router/Router.js';
+import { AppCommandRouter } from '../router/AppCommandRouter.js';
 
+export interface AppCommandHandlerOptions {
+  /**
+   * The client ID of the Discord Application
+   */
+  clientId: string;
+  /**
+   * The token of the Discord Application
+   */
+  token: string;
+}
+
+/**
+ * The AppCommandHandler class is used to handle commands for a Discord Application.
+ * @template Args The arguments that the command function will take
+ */
 export class AppCommandHandler<Args extends unknown[]> {
-  private readonly router: Router<Args>;
+  private readonly router: AppCommandRouter<Args>;
   private readonly clientId: string;
   private readonly token: string;
 
-  constructor(router: Router<Args>, { clientId, token }: { clientId: string; token: string }) {
+  /**
+   * @param {AppCommandRouter<Args>} router The router to use for command routing
+   * @param {AppCommandHandlerOptions} clientConfig
+   */
+  constructor(router: AppCommandRouter<Args>, clientConfig: AppCommandHandlerOptions) {
     this.router = router;
-    this.clientId = clientId;
-    this.token = token;
+    this.clientId = clientConfig.clientId;
+    this.token = clientConfig.token;
   }
 
+  /**
+   * Overwrite the commands for the Discord Application
+   * @returns {Promise<void>}
+   */
   public async overwriteCommands(): Promise<void> {
     try {
       await ky.put(`https://discord.com/api/v10/applications/${this.clientId}/commands`, {
@@ -25,6 +48,12 @@ export class AppCommandHandler<Args extends unknown[]> {
     }
   }
 
+  /**
+   * Run a command
+   * @param {string} commandName - The name of the command to run. This should be the name of the command as defined in the Discord API.
+   * @param {AppCommandType} commandType - The type of the command to run. This should be the type of the command as defined in the Discord API.
+   * @param args - The arguments to pass to the command function. Typically this will include an Interaction from Discord.
+   */
   public async run(commandName: string, commandType: AppCommandType, ...args: Args): Promise<void> {
     const commandFn = this.router.get(commandName, commandType);
 
