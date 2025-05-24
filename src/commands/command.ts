f5ca145eebd43f6d985bda480d2555aa070b8403
Jaren Goldberg <fyrlex@gmail.com>
2025-05-23 23:22:44 -0400

    fix: format

--- src/commands/command.ts
+++ src/commands/command.ts
@@ -32,7 +32,7 @@ export class Command<CT extends CacheType = CacheType> {
    */
   public async run(interaction: ChatInputCommandInteraction<CT>): Promise<void> {
     await interaction.deferReply({
-      flags: this.ephemeral ? (1 << 6) : undefined,
+      flags: this.ephemeral ? 1 << 6 : undefined,
     });
 
     await this.execute(interaction);
